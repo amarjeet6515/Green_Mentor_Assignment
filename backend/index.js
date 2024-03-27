63626f6a1797bf6f9d39cad0c3a331e00f5b0850
Amarjeet Ray <amarjeetray6515@gmail.com>
2024-03-27 09:53:03 +0530

    bckend

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+const tasksRoute = require("./routes/task.js");
+const authRoute = require("./routes/auth.js");
+const authMiddleware = require("./middleware/authMiddleware");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Middleware
+// app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+// Routes
+app.use("/api/tasks", authMiddleware, tasksRoute); // Protect tasks routes with JWT middleware
+app.use("/api/auth", authRoute);
+
+// MongoDB connection
+mongoose
+	.connect(process.env.mongodb_url)
+	.then(() => console.log("Connected to MongoDB"))
+	.catch((err) => console.error("Error connecting to MongoDB:", err));
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send("Something went wrong!");
+});
+
+// Start server
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
